8d3a227f2caf5b2bdd13d9ee681e6506ecca2b42
Arman Jordan <arajorda@ucsc.edu>
2023-11-05 19:54:03 -0800

    first playable ver.

--- crisp-game-lib-master/docs/Game/main.js
+++ crisp-game-lib-master/docs/Game/main.js
@@ -25,29 +25,67 @@ options = {
 const G = {
 	WIDTH: 100,
 	HEIGHT: 150,
-  SPAWNRATE: 10
+  SPAWNRATE: 50,
+  OBSTACLEMOVERATE: 0.5
 }
 
 let horizontalVelocity = 0;
 let verticalLevel = 0;
+let spawn = G.SPAWNRATE;
 let pin;
 let player;
-let obstacles;
+let obstacles = [];
 let counter = 0;
+
+let haystack = [];
+let timer = 10;
 
 function update() {
   if (!ticks) {
-    
+  }
+
+  timer--;
+
+  if (timer == 0){
+    haystack.push ({
+      pos: vec(G.WIDTH/2, 0)
+    });
+    haystack.forEach((hay) => {
+      hay.pos.y += 0.1;
+      //color("yellow");
+      box(hay.pos, 2);
+    });
   }
 
   player = {
     pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
-    angle: 0,
+    angle: 0
   };
+  color("black");
   player.pos = vec(G.WIDTH * 0.5 + horizontalVelocity, (G.HEIGHT - 60) + verticalLevel);
-  char("a", player.pos, {rotation: horizontalVelocity / 90});
-  //Spawning "Haystack"
+  char("a", player.pos);
 
+  //Spawning "Haystack"s
+
+  spawn -= 1;
+  if (spawn <= 0) {
+    // Create the bullet
+    obstacles.push({
+        pos: vec(player.pos.x, 0)
+    });
+    // Reset the firing cooldown
+    spawn = G.SPAWNRATE;
+  }
+  
+  obstacles.forEach((fb) => {
+    // Move the bullets down
+    fb.pos.y += G.OBSTACLEMOVERATE;
+    
+    // Drawing
+    color("yellow");
+    box(fb.pos, 5);
+  });
+  color("black");
 
 
   if (input.isPressed) {
@@ -58,24 +96,12 @@ function update() {
     horizontalVelocity -= 1
   }
   
+  // playing with obstacle spawn
   verticalLevel -= 0.1
 
-  obstacles = [];
-  
-  obstacles.forEach((fb) => {
-    // Move the bullets upwards
-    fb.pos.y -= G.FBULLET_SPEED;
-    
-    // Drawing
-    color("yellow");
-    box(fb.pos, 2);
-  });
-
   const isCollidingWithObs = char("a", player.pos).isColliding.rect.yellow;
-
-        // Check whether to make a small particle explosin at the position
-        if (isCollidingWithFBullets) {
-            color("yellow");
-            particle(e.pos);
-        }
+    // Check whether to make a small particle explosin at the position
+    if (isCollidingWithObs) {
+      verticalLevel += 3;
+    }
 }
