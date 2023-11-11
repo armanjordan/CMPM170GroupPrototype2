ffa4d5d6e85bd8642a9454d3d2f001e9a39d1182
timpt0261 <rchavez0215@gmail.com>
2023-11-10 20:34:10 -0800

    Added Restart

--- crisp-game-lib-master/docs/Game/main.js
+++ crisp-game-lib-master/docs/Game/main.js
@@ -1,19 +1,19 @@
 title = "Crazy Truck";
-
+// note changing the description changes the sfx
 description = `
-  [Hold] <-
-  [Release] ->
+  [Hold] ->
+  [Release] <-
 `;
 
 characters = [
-`
+  `
 LrrrrL
 LrrrrL
  rccr
  rccr 
 LrrrrL
 LrrrrL
-`
+`,
 ];
 
 options = {
@@ -23,11 +23,11 @@ options = {
 };
 
 const G = {
-	WIDTH: 100,
-	HEIGHT: 150,
+  WIDTH: 100,
+  HEIGHT: 150,
   SPAWNRATE: 50,
-  OBSTACLEMOVERATE: 0.5
-}
+  OBSTACLEMOVERATE: 0.5,
+};
 
 let horizontalVelocity = 0;
 let verticalLevel = 0;
@@ -40,71 +40,97 @@ let counter = 0;
 let haystack = [];
 let timer = 60;
 
+// Initialization function
+function initializeGame() {
+  // Reset player position and properties
+  player = {
+    pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
+    angle: 0,
+  };
+
+  // Reset other game variables
+  horizontalVelocity = 0;
+  verticalLevel = 0;
+  spawn = G.SPAWNRATE;
+  obstacles = [];
+  counter = 0;
+  haystack = [];
+  timer = 60;
+}
+
+// Example usage: restart the game when a key is pressed
+if (input.isJustPressed) {
+  initializeGame();
+}
+
 function update() {
   if (!ticks) {
   }
 
-  
   timer--;
-  if (timer == 0){
+  if (timer == 0) {
     addScore(1);
     timer = 60;
   }
 
   player = {
     pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
-    angle: 0
+    angle: 0,
+    alive: true,
   };
-  color("black");
+
+  // Player update
   player.pos.x = clamp(player.pos.x + horizontalVelocity, 0, G.WIDTH);
   player.pos.y = clamp(player.pos.y + verticalLevel, 0, G.HEIGHT);
+  color("black");
   char("a", player.pos);
 
-  if(player.pos.x == 0 || player.pos.x == G.WIDTH){
+  if (player.pos.x == 0 || player.pos.x == G.WIDTH) {
     play("hit");
-    verticalLevel += (player.pos.x === 0) ?1 :-1;
+    verticalLevel += player.pos.x === 0 ? 1 : -1;
   }
 
-  if (player.pos.y > 100 ){
+  // Handle Death
+  if (player.pos.y > 100) {
     end();
+    initializeGame();
+    return;
   }
 
   spawn -= 1;
   if (spawn <= 0) {
     // Create the bullet
     obstacles.push({
-        pos: vec(player.pos.x, 0)
+      pos: vec(player.pos.x, 0),
     });
     // Reset the firing cooldown
     spawn = G.SPAWNRATE;
   }
-  
+
   obstacles.forEach((fb) => {
     // Move the bullets down
     fb.pos.y += G.OBSTACLEMOVERATE;
-    
+
     // Drawing Haystaks
     color("yellow");
     box(fb.pos, 5);
   });
   color("black");
-  
 
-  if (input.isPressed) {
-    player.angle += player.angle == 90 ? 1 : 0;
-    horizontalVelocity += 1
+  if (keyboard.code.ShiftLeft.isPressed || keyboard.code.ShiftRight.isPressed) {
+    // player.angle += player.angle == 90 ? 1 : 0;
+    horizontalVelocity += 1;
   } else {
-    player.angle -=  player.angle == -90 ? 1: 0;
-    horizontalVelocity -= 1
+    // player.angle -= player.angle == -90 ? 1 : 0;
+    horizontalVelocity -= 1;
   }
-  
   // playing with obstacle spawn
-  verticalLevel -= 0.1
+  verticalLevel -= 0.1;
 
-  const isCollidingWithObs = char("a", player.pos).isColliding.rect.yellow;
-    // Check whether to make a small particle explosin at the position
-    if (isCollidingWithObs) {
-      verticalLevel += 3;
-      play("explosion");
-    }
+   const isCollidingWithObs = char("a", player.pos).isColliding.rect.yellow;
+   // Check whether to make a small particle explosin at the position
+   if (isCollidingWithObs) {
+     verticalLevel += 3;
+     play("explosion");
+   }
 }
